d18bd40dad4f0a01cc3edd9150c333b29bc4f93e
yeexiong17 <wyeexiong17@gmail.com>
2023-01-03 21:35:22 +0800

    give choice for player to choose their sign

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let start = document.getElementById("start");
 let blur = document.querySelector(".main");
 let startPage = document.querySelector(".start-page");
 let winPage = document.querySelector(".win-page");
+let check1 = document.getElementsByName("player-1");
+let check2 = document.getElementsByName("player-2");
+let radio1 = document.querySelectorAll(".radio1");
+let radio2 = document.querySelectorAll(".radio2");
 
 const gameBoard = [
     ["", "", ""],
@@ -15,12 +19,14 @@ const gameBoard = [
 
 const Play = () => {
 
+    // Player 1 info
     const player1 = {
         name: "Player 1",
         turn: true,
         sign: "x"
     };
 
+    // Player 2 info
     const player2 = {
         name: "Player 2",
         turn: false,
@@ -98,7 +104,7 @@ const Play = () => {
         // Check Row
         for (let i = 0; i < 3; i++) {
             if (gameBoard[i][0] != "" && gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][0] == gameBoard[i][2]) {
-                console.log(gameBoard[i][0]);
+                console.log(gameBoard[i][0] + " wins");
 
                 return true;
             }
@@ -107,7 +113,7 @@ const Play = () => {
         // Check Column
         for (let j = 0; j < 3; j++) {
             if (gameBoard[0][j] != "" && gameBoard[0][j] == gameBoard[1][j] && gameBoard[1][j] == gameBoard[2][j]) {
-                console.log(gameBoard[0][j]);
+                console.log(gameBoard[0][j] + " wins");
 
                 return true;
             }
@@ -115,12 +121,12 @@ const Play = () => {
 
         // Check Diagonal
         if (gameBoard[0][0] != "" && gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2]) {
-            console.log(gameBoard[0][0]);
+            console.log(gameBoard[0][0] + " wins");
 
             return true;
         }
         else if (gameBoard[0][2] != "" && gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][0]) {
-            console.log(gameBoard[0][2]);
+            console.log(gameBoard[0][2] + " wins");
 
             return true;
         }
@@ -161,12 +167,13 @@ function delay(time) {
 
 async function playAgain() {
 
-    await delay(800);
+    await delay(1000);
     winPage.style.display = "flex";
     blur.classList.add("blur-screen");
 
 }
 
+// When play again is clicked, everything refresh
 function restart() {
 
     winPage.style.display = "none";
@@ -191,40 +198,74 @@ function restart() {
 
     display1.style.display = "block";
     display2.style.display = "none";
-    display1.textContent = run.player1.name + "'s turn [x]";
-    display2.textContent = run.player2.name + "'s turn [o]";
+    display1.textContent = `${run.player1.name}'s turn [${run.player1.sign}]`;
+    display2.textContent = `${run.player2.name}'s turn [${run.player2.sign}]`;
 }
 
+// Start and sign selection page
 function starting() {
-    startPage.classList.remove("slide-in");
-    startPage.classList.add("slide-out");
+    if (run.player1.sign == run.player2.sign) {
+        alert("Choose Your Sign Again!");
+    }
+    else {
+        startPage.classList.remove("slide-in");
+        startPage.classList.add("slide-out");
 
-    playAgain2();
-    function delay(time) {
-        return new Promise(resolve => setTimeout(resolve, time));
+        // Wait 150ms to set start page display to none
+        playAgain2();
+        function delay(time) {
+            return new Promise(resolve => setTimeout(resolve, time));
+        }
+
+        async function playAgain2() {
+
+            await delay(150);
+            startPage.style.display = "none";
+
+        }
     }
 
-    async function playAgain2() {
+}
 
-        await delay(150);
-        startPage.style.display = "none";
+// Assign the selected player sign into object
+function playerSign() {
+    for (let i = 0; i < 3; i++) {
+
+        if (check1[i].checked && check1[i].value != "c") {
+            let newSign1 = check1[i].value;
+            run.player1.sign = newSign1;
+
+            display1.textContent = `${run.player1.name}'s turn [${run.player1.sign}]`;
+        }
+
+        if (check2[i].checked && check2[i].value != "c") {
+            let newSign2 = check2[i].value;
+            run.player2.sign = newSign2;
+
+            display2.textContent = `${run.player2.name}'s turn [${run.player2.sign}]`;
+        }
 
     }
-
 }
 
 // Main Code
 let run = Play();
 
-display1.textContent = run.player1.name + "'s turn [x]";
-display2.textContent = run.player2.name + "'s turn [o]";
-
 start.addEventListener('click', starting);
+
+radio1.forEach(check1 => {
+    check1.addEventListener('click', playerSign);
+});
+
+radio2.forEach(check2 => {
+    check2.addEventListener('click', playerSign);
+});
 
 clickBox.forEach(box => {
     box.addEventListener('click', myFunction);
 });
 
 again.addEventListener('click', restart);
+
 
 
